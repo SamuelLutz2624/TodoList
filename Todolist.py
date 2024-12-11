3aede0666fca25473e08323fb4506d03edbd6357
Samuel Lutz <lutzsamj2624@aguafria.local>
2024-12-11 08:39:35 -0700

    Not finished

---
+++ Todolist.py
@@ -0,0 +1,12 @@
+# This is how you make a list
+myList = ["thing 1", "thing 2", "thing 3", "thing 4"]
+# This is how you access individual items in the list
+print(myList[1])
+print(myList[3])
+# This is how you add to the end of the list
+myList.append("Thing 5")
+# This is how you print the entire list
+print(myList)
+# This is how you remove items using the index
+myList.pop(2)
+print(myList)
